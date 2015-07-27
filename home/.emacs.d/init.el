6592085fc416063814da8f8c1131062de2fb0f6c
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2015-07-27 21:58:35 +0200

    Auto-cleanup whitespace

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -66,3 +66,17 @@
 (setq yas-snippet-dirs
       '("~/.emacs.d/snippets"))
 (yas-global-mode 1)
+
+(setq whitespace-style '(
+                         face
+                         tabs
+                         trailing
+                         lines-tail
+                         space-before-tab
+                         newline
+                         empty
+                         space-after-tab
+                         tab-mark
+                         newline-mark))
+(add-hook 'prog-mode-hook 'whitespace-mode)
+(add-hook 'before-save-hook 'whitespace-cleanup)
