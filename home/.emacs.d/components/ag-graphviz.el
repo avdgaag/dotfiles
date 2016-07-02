290062a44b9f0b8b3938363be8f1d7d353ccf503
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2016-07-02 22:02:36 +0200

    Add Emacs graphviz

---
+++ home/.emacs.d/components/ag-graphviz.el
@@ -0,0 +1,35 @@
+;;; ag-graphviz.el --- Configure dot mode
+;;
+;; Author: Arjan van der Gaag <arjan@arjanvandergaag.nl>
+;; URL: http://arjanvandergaag.nl
+;; Version: 1.0.0
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; TODO
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+(use-package graphviz-dot-mode
+  :ensure t)
+
+(provide 'ag-graphviz)
+;;; ag-graphviz.el ends here
