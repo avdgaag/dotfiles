e49d7c7afb06f30bcf63e239d4a4090b625f5d29
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2016-07-02 22:07:30 +0200

    Use melpa stable

--- home/.emacs.d/components/ag-packages.el
+++ home/.emacs.d/components/ag-packages.el
@@ -31,7 +31,7 @@
 (require 'package)
 
 (add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
+       '("melpa" . "http://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
        '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
