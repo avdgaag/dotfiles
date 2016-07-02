20da6ff622d7c45496c27176ea1d699e196d33a1
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2016-07-02 22:16:18 +0200

    Clean up init.el

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -50,6 +50,7 @@
 (require 'ag-org)
 (require 'ag-paredit)
 (require 'ag-projectile)
+
 (require 'ag-ruby)
 (require 'ag-sh)
 (require 'ag-ui)
@@ -70,31 +71,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
  '(custom-safe-themes
    (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "f4020e085253f630b9dedb0bb2bea7dc574100b7993cac011f94097c4f92fd13" default)))
- '(safe-local-variable-values
-   (quote
-    ((eval progn
-           (require
-            (quote projectile))
-           (puthash
-            (projectile-project-root)
-            "bin/nanoc compile" projectile-compilation-cmd-map))
-     (flycheck-eslint-executable . "/Users/arjan/code/vecozo/node_modules/eslint/bin/eslint.js")
-     (flycheck-eslint-executable . /Users/arjan/code/vecozo/node_modules/eslint/bin/eslint\.js)
-     (flycheck-disabled-checkers . javascript-jshint)
-     (projectile-project-test-cmd . "mocha --no-colors --reporter dot"))))
- '(sql-product (quote postgres)))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "f4020e085253f630b9dedb0bb2bea7dc574100b7993cac011f94097c4f92fd13" default))))
+
+;; Use the Smyx color scheme
+(load-theme 'smyx)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#282828" :foreground "#F7F7F7" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "nil" :family "Source Code Pro for Powerline")))))
-
-;; Use the Smyx color scheme
-(load-theme 'smyx)
+ '(default ((t (:height 130 :family "Source Code Pro for Powerline")))))
 
 ;;; init.el ends here
