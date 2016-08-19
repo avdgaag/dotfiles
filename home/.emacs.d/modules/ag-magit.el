f4e967190c7d29acf9917e1c3701856d25f820e0
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2016-08-19 22:15:56 +0200

    Add v shortcut in Magit to open pull request

--- home/.emacs.d/modules/ag-magit.el
+++ home/.emacs.d/modules/ag-magit.el
@@ -11,6 +11,19 @@
   (setq magit-merge-arguments (quote ("--no-ff")))
   (setq magit-pull-arguments (quote ("--rebase")))
   (setq magit-push-arguments (quote ("--set-upstream")))
-  (setq magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256"))))
+  (setq magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
+  :config
+  (defun ag-visit-pull-request-url ()
+    "Visit the current branch's PR on Github."
+    (interactive)
+    (browse-url
+     (format "https://github.com/%s/pull/new/%s"
+             (replace-regexp-in-string
+              "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+              (magit-get "remote" "origin" "url"))
+             (magit-get-current-branch))))
 
+  (eval-after-load 'magit
+    '(define-key magit-mode-map "v"
+       #'ag-visit-pull-request-url)))
 (provide 'ag-magit)
