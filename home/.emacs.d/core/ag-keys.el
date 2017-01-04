6bac475f16e81512ea6ceeff4cf5ac0a6e42b81b
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2017-01-04 07:53:16 +0100

    Use ⌥⌘W for word wrapping

--- home/.emacs.d/core/ag-keys.el
+++ home/.emacs.d/core/ag-keys.el
@@ -1,7 +1,7 @@
 (global-set-key (kbd "<C-s-268632070>") 'toggle-frame-fullscreen)
 (global-set-key (kbd "s-=") 'text-scale-increase)
 (global-set-key (kbd "s--") 'text-scale-decrease)
-(global-set-key (kbd "C-c w") 'toggle-truncate-lines)
+(global-set-key (kbd "M-s-∑") 'toggle-truncate-lines)
 (global-set-key (kbd "C-x \\") 'align-regexp)
 
 (provide 'ag-keys)
