b2bf0c7990bd8b76d740cd4dd64a7b23840a689c
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2017-01-04 07:50:59 +0100

    Tweak multiple cursors keyboard shortcuts

--- home/.emacs.d/modules/ag-multiple-cursors.el
+++ home/.emacs.d/modules/ag-multiple-cursors.el
@@ -4,8 +4,13 @@
   :ensure t
   :bind (("C-c m" . mc/edit-lines)
          ("C-c M" . mc/edit-ends-of-lines)
-         ("C-c ." . mc/mark-next-like-this)
-         ("C-c ," . mc/mark-previous-like-this)
-         ("C-c /" . mc/mark-all-like-this)))
+         ("s-]" . mc/mark-next-like-this)
+         ("s-[" . mc/mark-previous-like-this)
+         ("s-}" . mc/mark-all-like-this)
+         ("s-{" . mc/mark-all-dwim)
+         )
+  :init
+  (global-unset-key (kbd "M-<down-mouse-1>"))
+  (global-set-key (kbd "M-<mouse-1>") 'mc/add-cursor-on-click))
 
 (provide 'ag-multiple-cursors)
