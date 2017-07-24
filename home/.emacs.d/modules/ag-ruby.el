4907eea9da619ea4d127fdfc8f4597e9e2daf098
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2017-07-24 19:43:43 +0200

    Use either projectile-rails or projectile-hanami

--- home/.emacs.d/modules/ag-ruby.el
+++ home/.emacs.d/modules/ag-ruby.el
@@ -22,7 +22,7 @@
   (setq projectile-rails-expand-snippet nil)
   (setq projectile-rails-vanilla-command "bin/rails")
   (setq projectile-rails-spring-command "bin/spring")
-  (add-hook 'projectile-mode-hook 'projectile-rails-on))
+  (add-hook 'projectile-mode-hook 'projectile-rails-or-hanami-on))
 
 (use-package bundler
   :ensure t
@@ -83,5 +83,26 @@
   (add-hook 'enh-ruby-mode-hook 'minitest-mode)
   :config
   (minitest-install-snippets))
+
+(use-package projectile-hanami
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'projectile-mode-hook 'projectile-rails-or-hanami-on))
+
+(defun projectile-hanami-on ()
+  "Activate `projectile-hanami-mode`."
+  (if (projectile-hanami-applicable-p)
+      (projectile-hanami-mode +1)))
+
+(define-globalized-minor-mode projectile-hanami-global-mode
+  projectile-hanami-mode
+  projectile-hanami-on)
+
+(defun projectile-rails-or-hanami-on ()
+  "Activate either `projectile-rails-mode` or `projectile-hanami-mode`."
+  (if (projectile-hanami-applicable-p)
+      (projectile-hanami-mode +1)
+      (projectile-rails-on)))
 
 (provide 'ag-ruby)
