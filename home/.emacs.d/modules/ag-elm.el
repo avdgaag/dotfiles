018a0a6b6d81db99245ff5c9b637b3c0a940f95e
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2017-07-24 19:40:49 +0200

    Add Elm snippets

--- home/.emacs.d/modules/ag-elm.el
+++ home/.emacs.d/modules/ag-elm.el
@@ -8,6 +8,7 @@
   (setq elm-format-on-save t)
   (setq elm-indent-offset 4)
   (setq elm-tags-on-save t)
+  (setq elm-sort-imports-on-save t)
   :config
   (add-to-list 'company-backends 'company-elm))
 
