2bc58d2a8c3f30e9a6bfa77443c8743a07b9cf42
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2023-02-17 20:37:02 +0100

    Add Doom config

---
+++ home/.doom.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((rspec-spec-command . "bin/rspec")
+     (rspec-use-bundler-when-possible))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
